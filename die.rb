406bb623dba50e28d22a588dfab6b952ce36f137
Chris <chris@adadevelopersacademy.org>
2017-02-21 12:10:27 -0800

    Finished assert-style-testing

---
+++ die.rb
@@ -0,0 +1,10 @@
+
+class Die
+
+  attr_reader :showing
+
+  def initialize
+    @showing = 0
+  end
+
+end
--- die_test.rb
+++ die_test.rb
@@ -2,5 +2,28 @@
 
 require 'minitest/autorun'
 require 'minitest/reporters'
+Minitest::Reporters.use!
 
 require_relative 'die'
+
+class TestDie < Minitest::Unit::TestCase
+
+  def test_creation_of_die
+    die = Die.new
+    assert die.class == Die, "A Die cannot be created as an instance of Die"
+  end
+
+  def test_showing_number_must_be_less_than_7
+    die = Die.new
+    assert die.showing < 7
+  end
+
+
+    def test_showing_number_must_be_greater_than_0
+      die = Die.new
+      assert_operator die.showing, :>, 0
+    end
+
+
+
+end
